d96ba8426a6f37292219d53dff0f42dad4e16923
azwanxd12 <122980478+azwanxd12@users.noreply.github.com>
2023-02-19 20:25:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -41,18 +41,18 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
+global.botname = "Azwanbot-Mdོ" //namabot kalian
+global.ownername = "ᴹᴿ᭄ wanzzOfcོ ×፝֟͜×" //nama kalian
+global.myweb = "https://s.id/wanzzofc" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/@wNźzOfc" //bebas asal jan hapus
 global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.email = "kiaaofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone list : https://www.php.net/manual/en/timezones.php 
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['62895412284006'] //premium user
+global.owner = ["62895412284006", "62821931157232"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "62895412284006" // nomor wa kalian
+global.ownernomerr = "+628964122840p6" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
